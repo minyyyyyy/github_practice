ec0e3e50fc0957e69cca8f82788539c157bdd6b1
minyyyyyy <dpyo20020909@gmail.com>
2024-07-23 02:06:46 +0900

    first commit

---
+++ src/main/java/com/mini/github_practice/GithubPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mini.github_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPracticeApplication.class, args);
+    }
+
+}
